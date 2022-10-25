48f61e63ea43bc35d6d0ee4e8e6f01d74a7690ee
cowhead9805 <cowhead9805@gmail.com>
2022-10-26 01:17:48 +0900

    third commit

--- midtest12.c
+++ midtest12.c
@@ -9,10 +9,10 @@ int main(void){
     int x = rand();
 
     if(x % 2 == 0)
-    printf("랜덤 생성된 %d는 짝수입니다.",x);
+    printf("랜덤 생성된 %d는 짝수입니다.\n",x);
 
     else
-    printf("랜덤 생성된 %d는 홀수입니다.",x);
+    printf("랜덤 생성된 %d는 홀수입니다.\n",x);
 
     return 0;
 }
